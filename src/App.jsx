0812b060a2d3f19b331e61f1215d374a921ef8d7
Joao Vargas <joao.gaiares@gmail.com>
2023-07-13 19:08:50 -0300

    feat: added more answers and loan options

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import React from "react";
 import ChatBot from "react-simple-chatbot";
 // import { saveAs } from "file-saver"; //
 
+function ApplyLoan() {
+  return (
+  <div> 
+    <a href="https://www.moneysupermarket.com/loans/">Click here for loan options</a> 
+  </div>
+  )
+}
+
 function Chatbot() {
   const waitForTrigger = ({previousValue}) => {
     if (previousValue === "Hello" || previousValue === "hello"){
@@ -11,11 +19,11 @@ function Chatbot() {
     }
 
     if (previousValue === "Good" || previousValue === "good"){
-      return "I'm good too! What you want?";
+      return "I'm good too! First, tell me your name.";
     }
 
     if (previousValue === "I want" || previousValue === "i want"){
-      return "Very good! What you want?";
+      return "Very good! Now, tell me your name.";
     }
 
     if (previousValue === "Goodbye" || previousValue === "goodbye"){
@@ -31,9 +39,29 @@ function Chatbot() {
       return "wait trigger"
     }
     
-    return "next"
+    return "set name"
   }
 
+  const setAnswerTrigger = ({value}) => {
+   const message = (value || '').toLowerCase();;
+    if (message.includes("loan")) {
+      return "loan trigger"
+    }
+
+    if (message.includes("i want")) {
+      return "want trigger"
+    }
+
+    if (message.includes("good")) {
+      return "good trigger"
+    }
+    if (message.includes("goodbye")) {
+      return "goodbye trigger"
+    }
+    return "unknown answer trigger";
+  }
+
+ 
   const steps = [
     {
       id: "Greet",
@@ -42,32 +70,55 @@ function Chatbot() {
     },
     { id: "wait trigger", user: true, trigger: "wait for trigger" },
     { id: "wait for trigger", message: waitForTrigger, trigger: setTrigger },
-    { id: "next", message: "PRóXIMO PASSO CARAI", end: true },
-    // { id: "Ask Name", message: "How is your name?", trigger: "waiting name" },
-    // { id: "waiting name", user: true, trigger: "Name"},
-    // {
-    //   id: "Name",
-    //   message: "Hi {previousValue}, please enter a password. We want privacy :)",
-    //   trigger: "waiting password"
-    // },
-    // { id: "waiting password", user: true, trigger: "anna write issue" },
-    // { id: "anna write issue", message: "Now, tell me what you want.", trigger: "write issue"},
-    // { id: "write issue", user: true, trigger: "next"},
-    // { id: "next", message: "{previousValue}"},
-    // {
-    //   id: "issues",
-    //   options: [
-    //     { value: "React", label: "React", trigger: "React" },
-    //     { value: "Node", label: "Node", trigger: "Node" }
-    //   ]
-    // },
-    // { id: "React", message: "Thanks for telling your react issue", end: true },
-    // { id: "Node", message: "Thanks for telling your node issue", end: true }
+    { id: "set name", user: true, trigger: "pre set password" },
+    { id: "pre set password", message: "Beautiful name, {previousValue}. Now, set a password.", trigger: "set password" },
+    
+    // ANNA ASKING STEPS
+  
+    { id: "set password", user: true, trigger: "anna ask" },
+    { id: "anna ask", message: "And what do you want?", trigger: "user ask" },
+
+    // USER ASK
+    { id: "user ask", user: true, trigger: setAnswerTrigger },
+
+    // UNKNOWN ANSWER
+
+    { id: "unknown answer trigger", message: "I don't know what you're saying, so goodbye.", end: true },
+
+     // LOAN ANSWERS
+    { id: "loan trigger", 
+        options: [ 
+          { 
+            value: "Do you want to apply for a loan?", 
+            label: "Do you want to apply for a loan?",
+            trigger: "apply loan"
+          },
+          { 
+            value: "Loan conditions", 
+            label: "Loan conditions",
+            trigger: "loan conditions"
+          },
+          { 
+            value: "Help", 
+            label: "Help",
+            trigger: "help"
+          },
+        ]
+    },
+    {id : "apply loan", component: <ApplyLoan />, end: true},
+    {id : "loan conditions", message: "loan conditions!!!", end: true},
+    {id : "help", message: "somebody help meee please!", end: true},
+
+    // WANT ANSWERS
+
+    { id: "want trigger", message: "ráa what do you want now????", end: true },
+    { id: "good trigger", message: "goood, very goooooood", end: true },
+    { id: "goodbye trigger", message: "Bye bye then", end: true },
   ];
 
   return (
     <div>
-      <ChatBot steps={steps} headerTitle="Conversar com Anna" speechSynthesis={{ enable: true, lang: 'en' }} recognitionEnable />
+      <ChatBot steps={steps} headerTitle="Chat with Anna" speechSynthesis={{ enable: true, lang: 'en' }} recognitionEnable />
     </div>
   );
 }
