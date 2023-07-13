7db651ba068041c9380cee52858553f07b901d3c
Joao Vargas <joao.gaiares@gmail.com>
2023-07-13 11:33:44 -0300

    chore: first commit startint the app, eslint and prettier configured

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+import './App.css'
+
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div>
+       Olá
+     </div>
+  )
+}
+
+export default App
