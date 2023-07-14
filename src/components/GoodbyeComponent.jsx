3385ac5e0962e22f47e20396a590c0c395197d7f
Joao Vargas <joao.gaiares@gmail.com>
2023-07-14 15:03:28 -0300

    feat: adds historic in goodbye summary screen

--- src/components/GoodbyeComponent.jsx
+++ src/components/GoodbyeComponent.jsx
@@ -1,28 +1,23 @@
 import { useEffect } from "react";
 
 function GoodbyeComponent({ steps }) {
-  console.log(steps);
   const { setName } = steps;
   const date = new Date();
   const formattedDate = date.toLocaleString();
   const username = setName ? setName.value : "unknown name";
 
+  const historic = JSON.parse(localStorage.getItem("historic"));
+
   useEffect(() => {
-    console.log("use effectzou");
-    const historic = JSON.parse(localStorage.getItem("historic"));
     const newRegister = {
-      username: username,
+      username,
       date: formattedDate,
     };
     if (historic) {
-      console.log("entrou no if");
-      console.log(historic);
-      console.log(newRegister);
       localStorage.setItem(
         "historic",
         JSON.stringify([...historic, newRegister])
       );
-      console.log(JSON.parse(localStorage.getItem("historic")));
     } else {
       localStorage.setItem("historic", JSON.stringify([newRegister]));
     }
@@ -41,15 +36,19 @@ function GoodbyeComponent({ steps }) {
       <div style={{ width: "100%" }}>
         <h3>Summary</h3>
         <table>
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Date</th>
+            </tr>
+          </thead>
           <tbody>
-            <tr>
-              <td>Username</td>
-              <td>Date</td>
-            </tr>
-            <tr>
-              <td>{username}</td>
-              <td>{formattedDate}</td>
-            </tr>
+            {historic.map((register) => (
+              <tr key={register.date}>
+                <td>{register.username}</td>
+                <td>{register.date}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
         <button type="button" onClick={exportHistoric}>
